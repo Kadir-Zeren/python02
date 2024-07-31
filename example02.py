2ec6ea1398b68f9a3139bed69ab66537a2ac4000
kadir-zeren <k4dzeren@gmail.com>
2024-07-31 11:39:10 +0300

    float int

--- example02.py
+++ example02.py
@@ -5,3 +5,163 @@ a = "bu benim yazdigim string \
 stringe devam ediyorum"
 a = "bu benim yazdigim string \nstringe devam ediyorum"
 print(a)
+planet = 'jupyter'
+price = 140
+pi_number = 3.14
+school = "clarusway"
+print(school)
+sayi= 35
+print (sayi)
+my_age = 33
+your_age = 30
+my_age = your_age
+print(my_age)
+variable = 3.14
+var_one = 'something'
+help('keywords')
+_okul = 'clarusway'
+print(_okul)
+variable = 'degisken'
+isim_soyisim = 'ali guzel'
+planet = 'jupyter'
+price = 140
+degisken = 'bu bir string'
+print(degisken)
+degisken = 5
+print(degisken)
+x = y = z = "same"
+print (x)
+print (y)
+print (z)
+a = 'degisken_1'
+b = 5
+c = 7.6
+print(a,b,c)
+january = march = may = july = august = october = december = 31
+# multi assignments in a single line
+april = june = september = november = 30
+february = 28
+print(january, february, march, april, may, june, july, august, september, october,
+november, december)
+ali = None
+print(ali)
+website = "apple.com"
+print(website)
+# assigning a new variable to website
+website = "clarusway.com"
+print(website)
+first_number = 100
+second_number = first_number
+print (second_number)
+x = 15
+y = 33
+Z = x
+x = y
+print (x)
+print (y)
+print(z)
+man = "andrew"
+color = "green"
+age = 32
+pi = 3.14
+color = "yellow"
+age = 44
+man = "joseph"
+print(man, age, color)
+
+
+my_text = 'being a good person'
+print(my_text)
+
+a = 'clarusway'
+print(type(a))
+
+sehir = '35'
+print(type(sehir))
+
+str_number = '1923'
+str_sign = '%(#&*?-•'
+print (str_number)
+print(str_sign)
+print(type(str_number), type(str_sign))
+my_integer = 40
+negative_num = -18
+print(my_integer)
+print (negative_num)
+sayi1 = 40
+print(type(sayi1))
+my_float = 40.0
+negative_float = -18.66
+print(my_float)
+print (negative_float)
+a=5.7
+print(type(a))
+b = True
+print(type(a), type(b))
+my_data = 'I am string'
+print (type(my_data) )
+a = 123
+print(type(a))
+print(str(a))
+yeni_a = str(a)
+print(type(yeni_a))
+
+pi = 3.14
+converted_pi = str(pi)
+print(converted_pi)
+print(type(converted_pi))
+
+print()
+a = 1.23
+print(type (a))
+print(int (a))
+print(int (234.987))
+
+no = 3
+converted_no = float(no)
+print (converted_no)
+print (type (converted_no))
+
+tamsayi = 7
+print(type (tamsayi))
+print(float (tamsayi))
+
+x = 39
+v = "11"
+y = "2.5"
+z = "I am at_"
+print(x-int(v))
+print(x-float(y))
+print(z+str(x))
+
+print(5 + 3)
+print(5 + 3.0)
+print( '123' + '12')
+
+number_int = 123
+number_flt = 1.23
+number_new = number_int + number_flt
+print("datatype of number_int:", type(number_int))
+print ("datatype of number_flt:", type(number_flt))
+print("Value of number_new:", number_new)
+print("datatype of number_new:", type(number_new))
+
+number_int = 123
+number_str = "456"
+print("Data type of number_int:",type(number_int))
+print("Data type of number_str:",type(number_str))
+
+
+
+number_int = 123
+number_str = "456"
+print ("Data type of number_int:", type(number_int))
+print("Data type of number_str before Type Casting:", type(number_str))
+
+number_str = int(number_str)
+print("Data type of number_Str after Type Casting:", type(number_str))
+
+number_sum = number_int + number_str
+
+print("Sum of number_int and number_str:", number_sum)
+print("Data type of the sum:", type (number_sum) )
